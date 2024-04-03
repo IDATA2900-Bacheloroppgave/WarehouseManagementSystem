62be2a41fe73a6564497b8f94ad2225474069a23
Mikkel Stavelie <staveliem@gmail.com>
2024-04-03 13:45:10 +0200

    added some boylerplate api doc

--- src/main/java/wms/rest/wms/api/controller/ProductController.java
+++ src/main/java/wms/rest/wms/api/controller/ProductController.java
@@ -1,10 +1,14 @@
 package wms.rest.wms.api.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import wms.rest.wms.model.OrderStatus;
 import wms.rest.wms.model.Product;
 import wms.rest.wms.service.ProductService;
 
@@ -14,6 +18,7 @@ import java.util.Optional;
 /**
  * Class represents the /products endpoint.
  */
+@Tag(name = "Products", description = "All endpoint operations related to Products.")
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
@@ -31,6 +36,11 @@ public class ProductController {
      *          Http status code 404 if list is empty.
      *          Http status code 200 if list is not empty.
      */
+    @Operation(summary = "Get a list of all products", description = "Returns a list of all products in database", responses = {
+        @ApiResponse(responseCode = "200", description = "Successful retrieval", content = @Content(schema = @Schema(implementation = Product.class))),
+        @ApiResponse(responseCode = "400", description = "Bad Request"),
+        @ApiResponse(responseCode = "404", description = "Product not found")
+    })
     @GetMapping
     public ResponseEntity<List<Product>> getProducts() {
         List<Product> products = this.productService.getProducts();
@@ -43,7 +53,6 @@ public class ProductController {
         }
         return response;
     }
-
 
 
     @GetMapping("{id}")
