75dd7f655914efd5af88a5d08aa49ec654f25b38
Mikkel Stavelie <staveliem@gmail.com>
2024-04-30 11:43:52 +0200

    added activeProfiles in ProductManagementSystemApplicationTests

--- src/test/java/wms/rest/wms/ProductManagementSystemApplicationTests.java
+++ src/test/java/wms/rest/wms/ProductManagementSystemApplicationTests.java
@@ -2,8 +2,10 @@ package wms.rest.wms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ProductManagementSystemApplicationTests {
 
 	@Test
